9d708ba9b1f83580d613bc7326794e9c9c8e2f09
neyadrian <neyadrian2018@gmail.com>
2026-03-24 21:22:57 -0300

    update

--- src/ServidorTCP.java
+++ src/ServidorTCP.java
@@ -4,7 +4,7 @@ import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Tcp {
+public class ServidorTCP {
     public void iniciar(int porta) throws IOException {
 
         ObjectOutputStream saida;
@@ -12,14 +12,24 @@ public class Tcp {
         boolean sair = false;
         String alimento = "";
 
-
         try {
             ServerSocket servidor = new ServerSocket(12000);
-
+            Socket conexao;
             while (!sair) {
                 Socket cliente = servidor.accept();
                 System.out.println("Cliente conectado: " + cliente.getInetAddress().getHostAddress());
 
+                saida = new ObjectOutputStream(conexao.getOutputStream());
+                entrada = new ObjectInputStream(conexao.getInputStream());
+
+                saida.writeObject("Conexão estabelecida!");
+
+                do {
+                    try {
+                        alimento = (String) entrada.readObject();
+                    }
+
+                } while (!alimento.equals("sair"));
             }
         }
 
