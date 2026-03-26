54856e98db44344e257a694caac51d7f9fa34c7d
neyadrian <neyadrian2018@gmail.com>
2026-03-25 21:10:29 -0300

    update - servidor udp

--- src/ServidorTCP.java
+++ src/ServidorTCP.java
@@ -1,21 +1,20 @@
 import java.io.*;
-import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class ServidorTCP {
-    public void iniciar(int porta) throws IOException {
+    public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.print("Digite o produto: ");
-        String produto = sc.nextLine();;
+        System.out.print("Digite o produto (TCP): ");
+        String produto = sc.nextLine();
 
         try (Socket socket = new Socket("Localhost", 12000)) {
             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
             out.println(produto);
-            System.out.println("Preço recebido: " + in.readLine());
+            System.out.println("Preço: " + in.readLine());
         } catch (IOException e) {
             System.err.println("Erro ao iniciar TCP.");
         }
--- src/ServidorUDP.java
+++ src/ServidorUDP.java
@@ -1,2 +1,33 @@
+import java.io.IOException;
+import java.net.*;
+import java.util.Scanner;
+
 public class ServidorUDP {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite o produto (UDP): ");
+        String produto = sc.nextLine();
+
+        try (DatagramSocket clienteSocket = new DatagramSocket()) {
+            clienteSocket.setSoTimeout(2000);
+
+            InetAddress IPAddress = InetAddress.getByName("localhost");
+            byte[] sendData = produto.getBytes();
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 12001);
+            clienteSocket.send(sendPacket);
+
+            byte[] receiveData = new byte[1024];
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+
+            clienteSocket.receive(receivePacket);
+            String resposta = new String(receivePacket.getData(), 0, receivePacket.getLength());
+            System.out.println("Preço: " + resposta);
+
+        } catch (SocketTimeoutException e) {
+            System.out.println("Resposta não recebida.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
